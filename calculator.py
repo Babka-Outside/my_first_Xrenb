1bc0245e8a948bdc81a361c1333dc9a42888f589
Student <Mishgun48@yandex.ru>
2023-04-21 16:54:25 +0300

    калькулятор

---
+++ calculator.py
@@ -0,0 +1,20 @@
+def fact(n):
+    res = 1
+    for i in range(2, n + 1):
+        res *= i
+    return res
+
+
+actions = {'+': lambda x, y: x + y,
+           '-': lambda x, y: x - y,
+           '*': lambda x, y: x * y,
+           '/': lambda x, y: x / y,
+           '!': fact}
+try:
+    print('Введите одно или два числа через пробел')
+    nums = map(int, input().split())
+    print('Введите действие: +, -, *, /, !.')
+    operation = input()
+    print(actions[operation](*nums))
+except Exception as error:
+    print(error)
